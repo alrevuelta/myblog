1d1a7a4416af8cf4c1168ae9732a72b4e3ee91db
Shankar Murralitharan <hello@shankar.me>
2025-02-04 00:11:42 +0530

    Add copy feature to codeblock
    Also as requested headers are no more uppercase

--- themes/alvaro/assets/js/main.js
+++ themes/alvaro/assets/js/main.js
@@ -2,13 +2,13 @@ var saved = localStorage.getItem("theme");
 if (!saved) saved = "💻";
 document.documentElement.setAttribute("data-theme", saved);
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('[value="' + saved + '"]').checked = true;
 
     var toggle = document.theme_switch.theme_radio;
 
     for (var i = 0; i < toggle.length; i++) {
-        toggle[i].addEventListener('change', function() {
+        toggle[i].addEventListener('change', function () {
             if (this.checked === true) {
                 console.log(saved);
                 document.documentElement.setAttribute("data-theme", this.value);
@@ -16,4 +16,41 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    document.querySelectorAll('.chroma:has(code)').forEach((codeblock) => {
+        const container = codeblock.parentNode;
+
+        const copybutton = document.createElement('button');
+        copybutton.classList.add('copy-code');
+        copybutton.innerHTML = 'copy';
+
+        function copyingDone() {
+            copybutton.innerHTML = 'done';
+            setTimeout(() => {
+                copybutton.innerHTML = 'copy';
+            }, 1000);
+        }
+
+        copybutton.addEventListener('click', (cb) => {
+            const code = codeblock.querySelectorAll("span.cl");
+            let codetext = "";
+
+            code.forEach((codeline) => {
+                codetext += codeline.textContent;
+            });
+
+            if ('clipboard' in navigator) {
+                navigator.clipboard.writeText(codetext);
+                copyingDone();
+            } else {
+                console.log("Clipboard api not supported");
+            }
+        });
+
+        if (container.classList.contains("highlight")) {
+            container.appendChild(copybutton);
+        } else {
+            codeblock.appendChild(copybutton);
+        }
+    });
 });
