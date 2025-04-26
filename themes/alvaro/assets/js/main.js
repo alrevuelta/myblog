3644080674ca5402caa036ace882c7e221cf2c6a
Alvaro Revuelta <alvrevuelta@gmail.com>
2025-04-26 23:35:07 +0200

    Update main.js

--- themes/alvaro/assets/js/main.js
+++ themes/alvaro/assets/js/main.js
@@ -5,18 +5,6 @@ document.documentElement.setAttribute("data-theme", saved);
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('[value="' + saved + '"]').checked = true;
 
-    var toggle = document.theme_switch.theme_radio;
-
-    for (var i = 0; i < toggle.length; i++) {
-        toggle[i].addEventListener('change', function () {
-            if (this.checked === true) {
-                console.log(saved);
-                document.documentElement.setAttribute("data-theme", this.value);
-                localStorage.setItem("theme", this.value);
-            }
-        });
-    }
-
     document.querySelectorAll('.chroma:has(code)').forEach((codeblock) => {
         const container = codeblock.parentNode;
 
